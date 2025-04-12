94fa6a923b7f1090953e7f29fcea6e61ec549917
pieterjansen8 <pieterjansen8@icloud.com>
2025-04-12 20:26:53 +0200

    1.1

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -2,6 +2,9 @@ import { env } from "$env/dynamic/private";
 import { json as retjson } from "@sveltejs/kit";
 export async function POST({request}: {request: Request}){
     const json = await request.json()
+    if(!json){
+        return retjson("No code given.")
+    }
     if(json.code==env.CODE){
         return retjson(true)
     }
